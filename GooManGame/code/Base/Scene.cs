1268cd399a32eba13b8225d5efe7d0f396bd686b
Gavin White <horace5413@gmail.com>
2020-11-01 18:56:03 -0800

    add Scene.cs

---
+++ GooManGame/code/Base/Scene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace GooManGame.code.Base {
+	/// <summary>
+	/// Discrete state of the game IE main menu, settings menu, play.
+	/// </summary>
+	public abstract class Scene {
+		#region updateables
+		List<IUpdateable> updateables;
+		public void AddUpdateable(IUpdateable ie) =>
+			updateables.Add(ie);
+		public void RemoveUpdateable(IUpdateable ie) =>
+			updateables.Remove(ie);
+		#endregion updateables
+
+		public virtual void Update(float elapsed) {
+			foreach (IUpdateable ie in updateables)
+				ie.Update(elapsed);
+		}
+	}
+}
