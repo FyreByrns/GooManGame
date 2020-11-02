cc3e2621c2128253550e71b52cdcd46a378ece4b
Gavin White <horace5413@gmail.com>
2020-11-01 19:07:35 -0800

    add main menu

---
+++ GooManGame/code/Base/sceneMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using PixelEngine;
+using static GooManGame.Debug;
+
+namespace GooManGame.code.Base {
+	public class sceneMainMenu : Scene {
+		public sceneMainMenu() {
+			Raise("Constructing Main Menu.");
+			AddUpdateable(new Title(new Vector2<float>(0, 0)));
+		}
+
+		public class Title : IUpdateable {
+			public Vector2<float> position;
+			Sprite sprite;
+
+			public void Update(float elapsed) {
+				Game.Instance.DrawSprite(position, sprite);
+			}
+
+			public Title(Vector2<float> topLeft) {
+				position = topLeft;
+				sprite = AssetManager.GetSprite("mainmenu_title");
+			}
+		}
+	}
+}
