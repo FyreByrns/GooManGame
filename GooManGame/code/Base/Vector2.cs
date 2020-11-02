8035b179b8b3e36201e4e27052ebd633377f068f
Gavin White <horace5413@gmail.com>
2020-11-01 18:28:49 -0800

    Clean Vector2.cs

--- GooManGame/code/Base/Vector2.cs
+++ GooManGame/code/Base/Vector2.cs
@@ -1,15 +1,38 @@
 ﻿namespace GooManGame {
+	/// <summary>
+	/// A 2D vector of structures. Only works properly with numeric types.
+	/// </summary>
 	public class Vector2<T> where T : struct {
+		/// <summary>
+		/// Values.
+		/// </summary>
 		public T x, y;
 
+		#region properties
+		/// <summary>
+		/// Squared length.
+		/// </summary>
 		public T Length2 => (dynamic)x * x + (dynamic)y * y;
+		/// <summary>
+		/// Length.
+		/// </summary>
 		public T Length => (dynamic)System.Math.Sqrt((dynamic)Length2);
 
+		/// <summary>
+		/// Normal before normalization.
+		/// </summary>
 		public Vector2<T> RawNormal =>
 			new Vector2<T>((dynamic)y, -(dynamic)x);
+		/// <summary>
+		/// Normalized normal.
+		/// </summary>
 		public Vector2<T> Normal =>
 			RawNormal.Normalized();
-
+		#endregion properties
+		#region methods (instance)
+		/// <summary>
+		/// This in normal space.
+		/// </summary>
 		public Vector2<T> Normalized() {
 			Vector2<T> result = new Vector2<T>();
 
@@ -21,20 +44,34 @@
 			return result;
 		}
 
+		/// <summary>
+		/// The dot product of this and another.
+		/// </summary>
 		public float DotProduct(Vector2<T> other) =>
 				DotProduct(this, other);
 
+		/// <summary>
+		/// Project this onto another vector.
+		/// </summary>
 		public Vector2<T> Project(Vector2<T> onto) =>
 				Project(this, onto);
-
+		#endregion methods (instance)
+		#region methods (static)
+		/// <summary>
+		/// Dot product of two vectors.
+		/// </summary>
 		public static float DotProduct(Vector2<T> va, Vector2<T> vb) =>
 				(dynamic)va.x * vb.x + (dynamic)va.y * vb.y;
 
+		/// <summary>
+		/// Project a v onto onto
+		/// </summary>
 		public static Vector2<T> Project(Vector2<T> v, Vector2<T> onto) {
 			float dp = DotProduct(v, onto);
 			return (dynamic)onto * dp;
 		}
-
+		#endregion methods (static)
+		#region operators
 		public static bool operator ==(Vector2<T> va, Vector2<T> vb) =>
 				va is object && vb is object &&
 				(dynamic)va.x == vb.x &&
@@ -62,7 +99,8 @@
 
 		public Vector2<TCast> Cast<TCast>() where TCast : struct =>
 			new Vector2<TCast>((TCast)(dynamic)x, (TCast)(dynamic)y);
-
+		#endregion operators
+		#region constructors
 		public Vector2() { }
 		public Vector2(Vector2<T> v) {
 			x = v.x;
@@ -76,11 +114,13 @@
 			this.x = x ?? default;
 			this.y = y ?? default;
 		}
-
+		#endregion constructors
+		#region standard
 		public override int GetHashCode() =>
 				base.GetHashCode();
 
 		public override bool Equals(object obj) =>
 				obj is Vector2<T> v && v == this;
+		#endregion standard
 	}
 }
