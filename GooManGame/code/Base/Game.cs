67219f2d2409330d412d9902647acc85bab3e46b
Gavin White <horace5413@gmail.com>
2020-11-01 18:31:00 -0800

    Document Game.cs

--- GooManGame/code/Base/Game.cs
+++ GooManGame/code/Base/Game.cs
@@ -1,5 +1,11 @@
 ﻿namespace GooManGame {
+	/// <summary>
+	/// Singleton, manages the main loop.
+	/// </summary>
 	public class Game : PixelEngine.Game {
+		/// <summary>
+		/// Instance of the game.
+		/// </summary>
 		public static Game Instance {
 			get {
 				if (_instance == null) _instance = new Game();
@@ -8,12 +14,18 @@
 		}
 		static Game _instance;
 
+		/// <summary>
+		/// Load configs and assets.
+		/// </summary>
 		public static void Preload() {
 			IO.LoadConfig();
 			AssetManager.Setup();
 			Instance.Construct(IO.ScreenWidth, IO.ScreenHeight, IO.ScreenScale, IO.ScreenScale, IO.FPSLock);
 		}
 
+		/// <summary>
+		/// Called every frame.
+		/// </summary>
 		public override void OnUpdate(float elapsed) {
 			base.OnUpdate(elapsed);
 		}
