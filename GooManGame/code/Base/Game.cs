d3b7e9400dca4ecaa637d2679443b88695245007
Gavin White <horace5413@gmail.com>
2020-11-01 18:28:40 -0800

    rename Game.Begin to Game.Preload, add Game.ctor

--- GooManGame/code/Base/Game.cs
+++ GooManGame/code/Base/Game.cs
@@ -1,22 +1,23 @@
 ﻿namespace GooManGame {
-    public class Game : PixelEngine.Game{
-        public static Game Instance {
-            get {
-                if (_instance == null) _instance = new Game();
-                return _instance;
-            }
-        }
-        static Game _instance;
+	public class Game : PixelEngine.Game {
+		public static Game Instance {
+			get {
+				if (_instance == null) _instance = new Game();
+				return _instance;
+			}
+		}
+		static Game _instance;
 
-        public static void Preload() {
-            IO.LoadConfig();
-            AssetManager.Setup();
-            Instance.Construct(IO.ScreenWidth, IO.ScreenHeight, IO.ScreenScale, IO.ScreenScale, IO.FPSLock);
-        }
+		public static void Preload() {
+			IO.LoadConfig();
+			AssetManager.Setup();
+			Instance.Construct(IO.ScreenWidth, IO.ScreenHeight, IO.ScreenScale, IO.ScreenScale, IO.FPSLock);
+		}
 
-        public override void OnUpdate(float elapsed) {
-            base.OnUpdate(elapsed);
+		public override void OnUpdate(float elapsed) {
+			base.OnUpdate(elapsed);
+		}
 
-        }
-    }
+		Game() { }
+	}
 }
