ba1c926af582a1f70d249c145e21c5c1a8121766
Gavin White <horace5413@gmail.com>
2020-11-01 18:50:47 -0800

    add IUpdateable.cs

---
+++ GooManGame/code/Base/IUpdateable.cs
@@ -0,0 +1,8 @@
+﻿namespace GooManGame.code.Base {
+	/// <summary>
+	/// Object which is contained within a <see cref="Scene"/> which is updated each frame.
+	/// </summary>
+	interface IUpdateable {
+		void Update(float elapsed);
+	}
+}
